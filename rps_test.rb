5073d9eeb35a6e6bc9f7c687017901f05808ff95
Sonya Khan <sonya.khan.cs@gmail.com>
2019-04-26 03:38:05 -0400

    R: Refactor to spec styled tests

--- rps_test.rb
+++ rps_test.rb
@@ -19,39 +19,26 @@ class Rules
 end
 
 class RPSTest < Minitest::Test
-  def test_rock_beats_scissors
-    rules = Rules.new
+
+  rules = Rules.new
+
+  describe 'when winning hand is played' do
     expected = :win
-    actual = rules.beats?(:rock, :scissors)
-    assert_equal expected, actual
+
+    it 'returns win' do
+      assert_equal(expected, rules.beats?(:rock, :scissors))
+      assert_equal(expected, rules.beats?(:paper, :rock))
+      assert_equal(expected, rules.beats?(:scissors, :paper))
+    end
   end
 
-  def test_paper_beats_rock
-    rules = Rules.new
-    expected = :win
-    actual = rules.beats?(:paper, :rock)
-    assert_equal expected, actual
-  end
-
-  def test_scissors_beats_paper
-    rules = Rules.new
-    expected = :win
-    actual = rules.beats?(:scissors, :paper)
-    assert_equal expected, actual
-  end
-
-  def test_rock_loses_to_rock
-    rules = Rules.new
+  describe 'when losing hand is played' do
     expected = :lose
-    actual = rules.beats?(:rock, :rock)
-    assert_equal expected, actual
-  end
 
-  def test_rock_loses_to_paper
-    rules = Rules.new
-    expected = :lose
-    actual = rules.beats?(:rock, :paper)
-    assert_equal expected, actual
+    it 'returns lose' do
+      assert_equal(expected, rules.beats?(:rock, :rock))
+      assert_equal(expected, rules.beats?(:rock, :paper))
+    end
   end
 
 end
